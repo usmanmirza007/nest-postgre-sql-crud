44047116175acbb05eaad91018cbdd9f042afda8
Muhammad Usman <usmandgk107@gmail.com>
2024-09-19 16:06:39 +0500

    Add controller or services in bookmark

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BookmarkController } from './bookmark.controller';
+import { BookmarkService } from './bookmark.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+    controllers: [BookmarkController],
+    providers: [BookmarkService, PrismaService]
+
+})
 export class BookmarkModule {}
