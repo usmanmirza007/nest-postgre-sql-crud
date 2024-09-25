1a4c2bd47e90bfd3544f3c3dadd2909410cf2742
Muhammad Usman <usmandgk107@gmail.com>
2024-09-25 04:59:09 -0500

    Upload image with multer

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,33 @@
 import { Controller, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName, imageFileFilter } from './validator';
+import { GetUser } from 'src/auth/decorator';
+import { UploadService } from './upload.service';
+
 @Controller('upload')
 export class UploadController {
+    constructor(
+        private uploadService: UploadService,
+    ) {}
+    
+    @Post()
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './file',
+            filename: editFileName
+        }),
+        fileFilter: imageFileFilter
+    }))
+
+    uploadFile(@UploadedFile(
+        new ParseFilePipe({
+            validators: [
+                new MaxFileSizeValidator({maxSize: 30000}),
+            ]
+        })
+    ) file: Express.Multer.File, @GetUser('id') userId: number) {
+        
+        this.uploadService.uploadFile(file, userId)
+    }
 }
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,5 +1,31 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UploadService {
+    constructor(private prisma: PrismaService) {}
+    
+
+    async uploadFile(file: Express.Multer.File, userId: number) {
+
+        const userExsit = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!userExsit) {
+            throw new NotFoundException('User not found')
+        }
+
+        this.prisma.user.update({
+            where: {
+                id: userExsit.id
+            },
+            data: {
+                ...userExsit,
+                picture: file.filename.toString()
+            }
+        })
+    }
 }
